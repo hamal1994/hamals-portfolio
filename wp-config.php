440f5a1a70a1dc7ebb01edb960cb27261711bb27
Hamal Patel <hamal@bigbluedoor.net>
2019-09-19 17:02:37 +0100

    delete wp-super-cache

--- wp-config.php
+++ wp-config.php
@@ -15,6 +15,9 @@
  * keep your config separate, we recommend using a 'wp-config-local.php' file,
  * which you should also make sure you .gitignore.
  */
+if (file_exists(dirname(__FILE__) . '/wp-config-local.php') && !isset($_ENV['PANTHEON_ENVIRONMENT'])):
+  # IMPORTANT: ensure your local config does not include wp-settings.php
+  require_once(dirname(__FILE__) . '/wp-config-local.php');
 
 /**
  * Pantheon platform settings. Everything you need should already be set.
