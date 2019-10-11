864fe5eb7a66a77ddeddc1217403c4d3a9240f97
Hamal Patel <hamal@bigbluedoor.net>
2019-10-11 18:19:41 +0100

    test

---
+++ wp-content/themes/custom/config.rb
@@ -0,0 +1,11 @@
+http_path = "/" #root level target path
+css_dir = "style/css" #targets our default style.css file at the root level of our theme
+sass_dir = "style/scss" #targets our sass directory
+ 
+# You can select your preferred output style here (can be overridden via the command line):
+output_style = :compressed
+ 
+# To enable relative paths to assets via compass helper functions.
+# note: this is important in wordpress themes for sprites
+ 
+relative_assets = true
