b6a11220e0e38182b83cf300f18045f1890f98a5
Hamal Patel <hamal@bigbluedoor.net>
2019-10-17 21:47:08 +0100

    print style css oct

--- wp-content/themes/custom/functions.php
+++ wp-content/themes/custom/functions.php
@@ -1,2 +1,6 @@
 <?php 
-        echo "AHAHAHAHA";
+function queueScripts() {
+        wp_register_style("style", get_stylesheet_directory_uri() . "/css/style.css", '', '1.0.0');
+        wp_enqueue_style('style');
+        } 
+        add_action( 'wp_enqueue_scripts', 'queueScripts' );
