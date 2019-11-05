f5793bcb017207a9a4f1197c04bf2b38fc18a7ca
hamalbigbluedoor <hamal@bigbluedoor.net>
2019-11-05 12:49:21 +0000

    set up importing of sass components

--- wp-content/themes/custom/functions.php
+++ wp-content/themes/custom/functions.php
@@ -1,6 +1,6 @@
 <?php 
 function queueScripts() {
-        wp_register_style("stylecustom", get_stylesheet_directory_uri() . "/style/css/style.css");
+        wp_register_style("stylecustom", get_stylesheet_directory_uri() . "/style/css/custom.css");
         wp_enqueue_style('stylecustom');
         } 
         add_action( 'wp_enqueue_scripts', 'queueScripts' );
