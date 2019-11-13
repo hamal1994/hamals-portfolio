4da976e39edec71ceda104d8cb78e9c0f33022e3
hamalbigbluedoor <hamal@bigbluedoor.net>
2019-11-13 16:29:52 +0000

    var dump

--- wp-content/plugins/name-directory-hamal/shortcode.php
+++ wp-content/plugins/name-directory-hamal/shortcode.php
@@ -264,6 +264,7 @@ function name_directory_show_directory($attributes)
         $index_letters = $starting_letters;
     }
 
+    $var_dump($attributes);
 
 
     foreach($index_letters as $index_letter)
